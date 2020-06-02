49a6ccd55dda21df935a7163ed79562f2e2f821d
stefanoMat <stefanokaefer@gmail.com>
2020-06-02 03:06:14 -0300

    test: ensure that pipeDriveRequest will be called

--- tests/src/service/controllers/integration.spec.js
+++ tests/src/service/controllers/integration.spec.js
@@ -1,13 +1,26 @@
 const IntegrationController = require('../../../../src/service/controllers/integration')
 const MissingParamError = require('../../../../src/service/errors/missingParamError')
 
+const makePipeDriveRequest = () => {
+  class PipeDriveRequest {
+    async get (value) {
+      return new Promise(resolve => resolve('valid_apiKey'))
+    }
+  }
+  return new PipeDriveRequest()
+}
+
 const makeSut = () => {
-  const sut = new IntegrationController()
-  return sut
+  const pipeDriveRequestStub = makePipeDriveRequest()
+  const sut = new IntegrationController(pipeDriveRequestStub)
+  return {
+    sut,
+    pipeDriveRequestStub
+  }
 }
 describe('IntegrationController', () => {
   test('Should return 400 if no apiKey is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
       }
@@ -15,5 +28,17 @@ describe('IntegrationController', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toEqual(400)
     expect(httpResponse.body).toEqual(new MissingParamError('apiKey'))
+  })
+
+  test('Should calls pipeDriveRequest with correct apiKey', async () => {
+    const { sut, pipeDriveRequestStub } = makeSut()
+    const httpRequest = {
+      body: {
+        apiKey: 'valid_apiKey'
+      }
+    }
+    const pipeDriveRequestSpy = jest.spyOn(pipeDriveRequestStub, 'get')
+    await sut.handle(httpRequest)
+    expect(pipeDriveRequestSpy).toHaveBeenCalledWith('valid_apiKey')
   })
 })
