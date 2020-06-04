6aaab68c188d1af69eb0bb473d34ebad8846c844
stefanoMat <stefanokaefer@gmail.com>
2020-06-03 22:49:56 -0300

    feat: add request that get all deals on 'won' status

--- src/infra/commands/pipeDriveRequest.js
+++ src/infra/commands/pipeDriveRequest.js
@@ -2,9 +2,63 @@ const axios = require('axios')
 
 class PipeDriveRequest {
   async get (apiKey) {
+    const organizations = []
     const url = `https://linkapi-sandbox3.pipedrive.com/v1/deals?status=won&start=0&api_token=${apiKey}`
     const response = await axios.get(url)
+    const orgs = response.data.related_objects.organization
+
+    for (const org in orgs) {
+      const id = orgs[org].id
+      const response = await this.getDetailsOrganization(apiKey, id)
+      organizations.push(response.data)
+    }
+    const keyFields = await this.getCustomFieldsOrganization(apiKey)
+    const deals = response.data.data
+    const structueDeals = this.makeStructuredDeal(deals, organizations, keyFields)
+    return structueDeals
+  }
+
+  async getDetailsOrganization (apiKey, id) {
+    const url = `https://linkapi-sandbox3.pipedrive.com/v1/organizations/${id}?api_token=${apiKey}`
+    const response = await axios.get(url)
     return response.data
+  }
+
+  makeStructuredDeal (deals, organizations, keyFields) {
+    const structuredDeals = []
+
+    for (const org in organizations) {
+      const orgObj = organizations[org]
+      const dealsOfOrg = []
+
+      for (const deal in deals) {
+        const dealObj = deals[deal]
+        if (dealObj.org_id.value === orgObj.id) {
+          const dealStructured = {
+            id: dealObj.id,
+            title: dealObj.title,
+            value: dealObj.value
+          }
+          dealsOfOrg.push(dealStructured)
+        }
+      }
+      const structured = {
+        id: orgObj.id,
+        name: orgObj.name,
+        cnpj: orgObj[keyFields.cnpj],
+        inscricaoEstdual: orgObj[keyFields.inscricaoEstdual],
+        address: orgObj.address_formatted_address,
+        address_street_number: orgObj.address_street_number,
+        address_sublocality: orgObj.address_sublocality,
+        address_postal_code: orgObj.address_postal_code,
+        address_admin_area_level_1: orgObj.address_admin_area_level_1,
+        address_admin_area_level_2: orgObj.address_admin_area_level_2,
+        cc_email: orgObj.cc_email,
+        itens: dealsOfOrg
+      }
+      structuredDeals.push(structured)
+    }
+    return structuredDeals
   }
 }
 module.exports = PipeDriveRequest
