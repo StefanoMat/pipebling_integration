7212f695cb07d89f188edaaf6d6edfdfe4e975e8
stefanoMat <stefanokaefer@gmail.com>
2020-06-04 20:30:13 -0300

    refactor: change the port fixed to .env

--- src/main/index.js
+++ src/main/index.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 const MongoHelper = require('../infra/db/helpers/mongoHelper')
 MongoHelper.connect(process.env.DB_URL)
   .then(async () => {
-    server.listen(3000, () => console.log(`Server running on ${3000}`))
+    server.listen(3000, () => console.log(`Server running on ${process.env.PORT}`))
   })
