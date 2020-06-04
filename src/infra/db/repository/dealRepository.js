eec14cdf1b1eb4b6decd2c48b5bc1c890ad9a4cd
stefanoMat <stefanokaefer@gmail.com>
2020-06-04 20:14:38 -0300

    feat: should return all deals aggregated by date and value

--- src/infra/db/repository/dealRepository.js
+++ src/infra/db/repository/dealRepository.js
@@ -6,5 +6,27 @@ class DealRepository {
     const result = await dealCollection.insertMany(dealModels)
     return result.ops
   }
+
+  async get () {
+    const dealCollection = await MongoHelper.getCollection('deals')
+    const result = await dealCollection.aggregate([
+      {
+        $group: {
+          _id: {
+            date: {
+              $dateToString: {
+                date: '$created_at',
+                format: '%Y-%m-%d'
+              }
+            }
+          },
+          valor_total: { $sum: '$valor_total' },
+          deals: { $push: '$$ROOT' }
+
+        }
+      }
+    ]).toArray()
+    return result
+  }
 }
 module.exports = DealRepository
