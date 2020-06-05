b13ad8f23d04bbd85e67b4a08799d4d615bcfdb2
stefanoMat <stefanokaefer@gmail.com>
2020-06-04 23:08:32 -0300

    chore: add .env file

--- src/main/index.js
+++ src/main/index.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 const MongoHelper = require('../infra/db/helpers/mongoHelper')
 MongoHelper.connect(process.env.DB_URL)
   .then(async () => {
-    server.listen(3000, () => console.log(`Server running on ${process.env.PORT}`))
+    server.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`))
   })
